15294325a36546bcdbe0abe0650dcdb1d149d458
unknown <cayelsie@gmail.com>
2021-01-13 12:43:04 -0500

    PyBank runs beautifully at this stage; just need to make it a text file.

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,30 +11,16 @@ change_list = []
 #List to store the dates that go with the monthly change calcs
 date_list = []
 
+#List to store the monthly changes and dates zipped together
 change_date_list = []
 
-#Write a function for calculating the average for monthly profit/loss changes. This is probably not going to be used in final version but works when I pull out the profit/loss data into its own list.
+#Write a function for calculating the average for monthly profit/loss changes - because I just want to include a function for practice!
 def average_change(numbers):
-    length = len(numbers)
     total = 0
-    for x in range(length):
-        if x < length - 1:
-            difference = numbers[x + 1] - numbers[x]
-            total += difference
-    return total/(length-1)
+    for number in numbers:
+        total = total + number
+    return round(total/(len(numbers)),2)
 
-#Write a function for calculating the monthly profit/loss change without the average. This will be stored in a separate list for later.
-def monthly_change(numbers):
-    length = len(numbers)
-    total = 0
-    for x in range(length):
-        if x < length -1:
-            difference = numbers[x + 1] - numbers[x]
-            monthly_changes.append(difference)
-            for y in range(0, len(monthly_changes)):
-                monthly_changes[y] = int(monthly_changes[y])
-   
-    print(f'{monthly_changes}')
 
 #Set variable for total months to 0 initially for the counter
 total_months = 0
@@ -74,13 +60,26 @@ with open(Pybank_csv, newline = '', encoding = 'utf-8') as csvfile:
 
 #Zip the list of dates and changes together
 change_date_list = zip(date_list, change_list)
-for dates in change_date_list:
-    print(dates)
 
+#Set initial variables for max increase and decrease
+max_increase = 0
+max_decrease = 0
+
+for date in change_date_list:
+    if date[1] > max_increase:
+        max_increase = date[1]
+        max_date = date[0]
+    if date[1] < max_decrease:
+        max_decrease =  date[1]
+        min_date = date[0] 
+    
     
 #Print output to terminal. Need to save everything into a text file output - is the output to the terminal from the text file or do I do as below??
 print(f'Financial Analysis')
 print(f'---------------------------')
 print(f'Total Months: {total_months}')
 print(f'Total: ${total_sum}')
-print(change_date_list)
+print(f'Average Change: ${average_change(change_list)}')
+print(f'Greatest Increase in Profits: {max_date} (${max_increase})')
+print(f'Greatest Decrease in Profits: {min_date} (${max_decrease})')
+
