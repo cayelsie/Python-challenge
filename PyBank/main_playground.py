5c799dd48d98f0af804fb6566767e5969d09f2ef
unknown <cayelsie@gmail.com>
2021-01-13 12:01:36 -0500

    This successfully stores monthly change, date lists and zips them

--- PyBank/main_playground.py
+++ PyBank/main_playground.py
@@ -1,3 +1,4 @@
+#Modules
 import os
 import csv
 
@@ -5,16 +6,14 @@ import csv
 Pybank_csv = os.path.join("..", "PyBank", "Resources", "budget_data.csv")
 
 #List to store the monthly changes?
+change_list = []
 
+#List to store the dates that go with the monthly change calcs
+date_list = []
 
-#write a function for summing the profits/losses
-def sum(numbers):
-    total = 0
-    for number in numbers:
-        total += number
-    return total
+change_date_list = []
 
-#Write a function for calculating the average for monthly profit/loss changes. This is probably not going to be used in final version but works to get the correct number.
+#Write a function for calculating the average for monthly profit/loss changes. This is probably not going to be used in final version but works when I pull out the profit/loss data into its own list.
 def average_change(numbers):
     length = len(numbers)
     total = 0
@@ -24,7 +23,7 @@ def average_change(numbers):
             total += difference
     return total/(length-1)
 
-#Write a function for calculating the monthly profit/loss change. This will be stored in a separate list for later
+#Write a function for calculating the monthly profit/loss change without the average. This will be stored in a separate list for later.
 def monthly_change(numbers):
     length = len(numbers)
     total = 0
@@ -41,6 +40,8 @@ def monthly_change(numbers):
 total_months = 0
 #Set variable for total sum to 0 initially
 total_sum = 0
+#Set variable for previous row to 0 initially
+previous_row = 0
 
 #open the file as csv
 with open(Pybank_csv, newline = '', encoding = 'utf-8') as csvfile:
@@ -51,15 +52,31 @@ with open(Pybank_csv, newline = '', encoding = 'utf-8') as csvfile:
     #Read header, store the header row 
     csvheader = next(csvfile)
  
-    #i don't know if the for j part works, but the for i works great to transform that list into integers. I doubt the for j part works.
+    #Loop through the rows in the list
     for row in csvreader:
-       #Start counter for counting rows: aka number of months
-        total_months = total_months + 1
 
         #loop through and calculate sum
         total_sum = total_sum + int(row[1])
-        
+
+        #subtract profit/loss value in previous row from value in profit/loss column in next row 
+        monthly_change = int(row[1]) - previous_row
+        if total_months >= 1:
+            change_list.append(monthly_change)
+            date_list.append(row[0])
+        previous_row = int(row[1])
+
+
+        #Start counter for counting rows: aka number of months
+        total_months = total_months + 1
+
+       #Can easily get an average from change_list and also search max/min values in that list...but how in the world do I do that in the original list to be able to match it up with original date?? Append original list with change data??
+
+change_date_list = zip(date_list, change_list)
+for dates in change_date_list:
+    print(dates)
+#Print output to terminal. Need to save everything into a text file output - is the output to the terminal from the text file or do I do as below??
 print(f'Financial Analysis')
 print(f'---------------------------')
 print(f'Total Months: {total_months}')
 print(f'Total: ${total_sum}')
+print(change_date_list)
