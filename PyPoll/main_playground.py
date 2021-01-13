8c11a78e9f29c32000a04c171ab20b58c224fefe
unknown <cayelsie@gmail.com>
2021-01-13 17:27:34 -0500

    Working on a PyPoll version that stores info in dictionaries instead of hard coding names from list. main_playground stores candidates, total votes and total votes for each candidate

--- PyPoll/main_playground.py
+++ PyPoll/main_playground.py
@@ -4,19 +4,23 @@ import csv
 #build relative file path
 Pypoll_csv = os.path.join("..", "PyPoll", "Resources", "election_data.csv")
 
-# Lists to store data
-voter_id = []
-county = []
+# List to hold names of candidates who received votes
 candidates = []
+
+#make a dictionary that will hold each candidate and their number of votes
+candidate_votes_dict = {}
 
 #Set initial variable for the number of votes
 total_votes = 0
 
-#Set initial variable for each candidate vote counter
-Khan = 0
-Correy = 0
-Li = 0
-O_Tooley = 0
+#Set initial variable for the winner's name
+winner = ""
+
+#Set intiial variable of winner's votes to 0
+winner_votes = 0
+
+#Set initial variable of winner's percentage to 0
+winner_percentage = 0
 
 #open the file as csv
 with open(Pypoll_csv, newline = '', encoding = 'utf-8') as csvfile:
@@ -34,25 +38,29 @@ with open(Pypoll_csv, newline = '', encoding = 'utf-8') as csvfile:
         #loop through and use a counter to calculate how many votes total
         total_votes = total_votes + 1
 
+        
+        #Set a value for the name of the candidate
+        candidate = row[2]
+
         #loop through and make a list of candidates that obtained votes, without duplicates
         if row[2] not in candidates:
-            candidates.append(row[2])
+            candidates.append(candidate)
+        
+        #Start tallying the votes for each candidate and store with candidate's name in dictonary
+            candidate_votes_dict[candidate] = 0
+        candidate_votes_dict[candidate] = candidate_votes_dict[candidate] + 1
 
-        #Count number of votes for each candidate
-        if row[2] == "Khan":
-            Khan = Khan + 1
-        if row[2] == "Correy":
-            Correy = Correy + 1
-        if row[2] == "Li":
-            Li = Li + 1
-        if row[2] == "O'Tooley":
-            O_Tooley = O_Tooley + 1
+        #
+ 
+
+print(candidate_votes_dict)
 
 #Calculate percent for each candidate
 Khan_percent = round((Khan/total_votes) * 100, 3)
 Correy_percent = round((Correy/total_votes) * 100, 3)
 Li_percent = round((Li/total_votes) * 100, 3)
 O_Tooley_percent = round((O_Tooley/total_votes) * 100, 3)
+
 
 
 print(f'Election Results')
